6f63a1d6a5adafb38e62e8d574087fccfb7d6e26
Dan Rammer <hamersaw@gmail.com>
2016-05-04 18:17:58 -0600

    added compute_tokens application and updated tokens in example configs

---
+++ src/bin/compute_tokens.rs
@@ -0,0 +1,23 @@
+extern crate argparse;
+use argparse::{ArgumentParser, Store};
+
+fn main() {
+    let mut size: u16 = 0u16;
+    {    //solely to limit scope of parser variable
+        let mut parser = ArgumentParser::new();
+        parser.set_description("Compute tokens for cluster of size n");
+        parser.refer(&mut size).add_option(&["-s", "--cluster_size"], Store, "Size of the cluster").required();
+        parser.parse_args_or_exit();
+    }
+
+    println!("Tokens for cluster of size {}", size);
+    let delta = (u64::max_value() / (size as u64)) as i64;
+    println!("delta: {}", delta);
+
+    let mut v = i64::min_value();
+    for i in 0..size {
+        println!("{}: {}", i, v);
+        //println!("{}: {}", i, delta * (i as u64));
+        v+=delta
+    }
+}
